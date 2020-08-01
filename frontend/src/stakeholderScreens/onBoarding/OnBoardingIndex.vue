74dd03bd5920c06e0becb99cd6b133e316c41aba
imKewal <52960418+kewalkaushik@users.noreply.github.com>
2020-08-01 16:34:52 +0530

    onboarding stakeholders

---
+++ frontend/src/stakeholderScreens/onBoarding/OnBoardingIndex.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <AppBar @toMiniVariant="changeMiniVariant" />
+        <SideBar :navItems="onBoardingNavItems" :miniVariant="miniVariant" />
+        <v-main app>
+            <v-container>
+                <router-view></router-view>
+            </v-container>
+        </v-main>
+
+        <Footer />
+    </div>
+</template>
+
+<script>
+import SideBar from '@/dashboard/layout/SideBar.vue'
+import AppBar from '@/dashboard/layout/AppBar.vue'
+import Footer from '@/dashboard/layout/Footer.vue'
+
+export default {
+    name: 'OnBoardingIndex',
+    methods: {
+        changeMiniVariant() {
+            this.miniVariant = !this.miniVariant
+        },
+    },
+    components: {
+        SideBar,
+        AppBar,
+        Footer,
+    },
+    data() {
+        return {
+            miniVariant: true,
+            onBoardingNavItems: [
+                {
+                    title: 'Dashboard',
+                    icon: 'fa-home',
+                    route: '',
+                },
+                {
+                    title: 'Add User',
+                    icon: '',
+                    route: { name: 'AddUser' },
+                },
+                {
+                    title: 'View All ',
+                    icon: '',
+                    route: '/onboarding/viewAll',
+                },
+                {
+                    title: 'All User Roles ',
+                    icon: 'fa-users ',
+                    route: { name: 'AllUserRoles' },
+                },
+            ],
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ frontend/src/stakeholderScreens/onBoarding/user/AddUser.vue
@@ -0,0 +1,100 @@
+<template>
+    <div>
+        <v-container app>
+            <v-form>
+                <h1>Add New User</h1>
+                <v-row>
+                    <v-col cols="3">
+                        <v-subheader>User Name</v-subheader>
+                    </v-col>
+                    <v-col cols="3">
+                        <v-text-field outlined></v-text-field>
+                    </v-col>
+                </v-row>
+
+                <v-row>
+                    <v-col cols="3">
+                        <v-subheader>Password</v-subheader>
+                    </v-col>
+                    <v-col cols="3">
+                        <v-text-field outlined></v-text-field>
+                    </v-col>
+                </v-row>
+
+                <v-row>
+                    <v-col cols="3">
+                        <v-subheader>First Name</v-subheader>
+                    </v-col>
+                    <v-col cols="3">
+                        <v-text-field outlined></v-text-field>
+                    </v-col>
+                </v-row>
+
+                <v-row>
+                    <v-col cols="3">
+                        <v-subheader>Last Name</v-subheader>
+                    </v-col>
+                    <v-col cols="3">
+                        <v-text-field outlined></v-text-field>
+                    </v-col>
+                </v-row>
+
+                <v-row>
+                    <v-col cols="3">
+                        <v-subheader>Email</v-subheader>
+                    </v-col>
+                    <v-col cols="3">
+                        <v-text-field outlined></v-text-field>
+                    </v-col>
+                </v-row>
+
+                <v-row>
+                    <v-col cols="3">
+                        <v-subheader>Contact Primary</v-subheader>
+                    </v-col>
+                    <v-col cols="3">
+                        <v-text-field outlined></v-text-field>
+                    </v-col>
+                </v-row>
+
+                <v-row>
+                    <v-col cols="3">
+                        <v-subheader>Contact Secondary</v-subheader>
+                    </v-col>
+                    <v-col cols="3">
+                        <v-text-field outlined></v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="3">
+                        <v-subheader>User Role</v-subheader>
+                    </v-col>
+                    <v-col cols="3">
+                        <v-select label="Service" outlined></v-select>
+                    </v-col>
+                </v-row>
+
+                <v-row>
+                    <v-col cols="3">
+                        <v-subheader>User Status</v-subheader>
+                    </v-col>
+                    <v-col cols="3">
+                        <v-select label="Service" outlined></v-select>
+                    </v-col>
+                </v-row>
+                <br />
+                <div class="text-center">
+                    <v-btn class="primary" to="viewall">Add User</v-btn>
+                </div>
+            </v-form>
+        </v-container>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AddUser',
+}
+</script>
+
+<style lang="scss" scoped></style>
