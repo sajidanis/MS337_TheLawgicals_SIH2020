13b18284efef0b9434263332b7d909c8cd8598e8
Vaibhav Rajput <vaibhav.rajput94@gmail.com>
2020-08-03 17:32:18 +0530

    Removed ocr-api directory

--- ocr-api/ocr.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import pytesseract
-import requests
-
-def ocr_text(url):
-    img = Image.open(requests.get(url, stream=True).raw)
-    text = pytesseract.image_to_string(img)
-    return text
